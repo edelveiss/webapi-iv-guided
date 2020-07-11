11ebc33f863fd8376ce5070d2d1c433aade14a63
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-07-10 20:42:55 -0700

    port || 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require("./api/server.js");
 
 // we'll read the port from the server environment if it is there
 // heroku will have the PORT environment variable set
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
 server.listen(port, () => {
